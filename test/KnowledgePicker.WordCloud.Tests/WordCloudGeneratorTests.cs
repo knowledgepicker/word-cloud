5827c73aa9d26eb5881cd08198eec4144642d853
Jan Joneš <jjones@outlook.cz>
2023-04-26 18:33:44 +0200

    Add a test

--- test/KnowledgePicker.WordCloud.Tests/WordCloudGeneratorTests.cs
+++ test/KnowledgePicker.WordCloud.Tests/WordCloudGeneratorTests.cs
@@ -1,8 +1,10 @@
+using KnowledgePicker.WordCloud.Coloring;
 using KnowledgePicker.WordCloud.Drawing;
 using KnowledgePicker.WordCloud.Layouts;
 using KnowledgePicker.WordCloud.Primitives;
 using KnowledgePicker.WordCloud.Sizers;
 using SkiaSharp;
+using System.Drawing;
 
 namespace KnowledgePicker.WordCloud.Tests;
 
@@ -35,5 +37,50 @@ public class WordCloudGeneratorTests
         // Assert.
         Assert.Equal(result1.AsEnumerable(), result2);
         Assert.Equal(2, result2.Length);
+    }
+
+    [Fact] // https://github.com/knowledgepicker/word-cloud/issues/23
+    public void CanGetColor()
+    {
+        // Arrange.
+        var wordCloud = new WordCloudInput(new[]
+        {
+            new WordCloudEntry("a", 1),
+            new WordCloudEntry("b", 1),
+            new WordCloudEntry("c", 1),
+        })
+        {
+            Width = 1024,
+            Height = 256,
+            MinFontSize = 8,
+            MaxFontSize = 32
+        };
+        var sizer = new LogSizer(wordCloud);
+        using var engine = new SkGraphicEngine(sizer, wordCloud);
+        var layout = new SpiralLayout(wordCloud);
+        var color1 = Color.FromArgb(0x0f3057);
+        var color2 = Color.FromArgb(0xe25a5a);
+        var colorizer = new SpecificColorizer(
+            new Dictionary<string, Color>
+            {
+                ["a"] = color1,
+                ["b"] = color2
+            });
+        var wcg = new WordCloudGenerator<SKBitmap>(wordCloud, engine, layout, colorizer);
+
+        // Act.
+        foreach (var (item, fontSize) in wcg.Arrange())
+        {
+            var actualColor = colorizer.GetColor(item);
+
+            // Assert.
+            Color? expectedColor = item.Entry.Word switch
+            {
+                "a" => color1,
+                "b" => color2,
+                _ => null
+            };
+            Assert.Equal(expectedColor, actualColor);
+        }
     }
 }
